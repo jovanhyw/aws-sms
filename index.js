60821469c43b940de284d2f3c4b684ef543b3562
Jovan Ho <jovan_ho@tech.gov.sg>
2022-09-13 11:19:11 +0800

    add variables

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const AWS = require('aws-sdk');
 AWS.config.update({ region: 'ap-southeast-1' });
 require('dotenv').config();
 
+const senderID = '';
+const message = `test msg from ${senderID}`;
+const recipientNumber = ''
+
 const params = {
-  Message: 'test msg',
-  PhoneNumber: '', // The phone number to send SMS to
+  Message: message,
+  PhoneNumber: recipientNumber, // The phone number to send SMS to
   MessageAttributes: {
     'AWS.SNS.SMS.SenderID': {
       'DataType': 'String',
-      'StringValue': '' // Sender ID value
+      'StringValue': senderID // Sender ID value
     }
   }
 };
